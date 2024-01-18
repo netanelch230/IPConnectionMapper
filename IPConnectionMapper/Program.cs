0cd3f8b7fad1ec630c8d46f0190f390fcc3d91b1
netanelka <netanelka@checkpoint.com>
2024-01-18 16:13:19 +0200

    Write details to json file

--- IPConnectionMapper/Program.cs
+++ IPConnectionMapper/Program.cs
@@ -2,17 +2,40 @@
 using System.Linq;
 using Nmap.NET;
 using Nmap.NET.Container;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Formats.Asn1;
 
 namespace IPConnectionManager
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        public static void Main()
         {
             var target = new Target("127.0.0.1");
             Console.WriteLine("Initializing scan of {0}", target);
             ScanResult result = new Scanner(target, System.Diagnostics.ProcessWindowStyle.Hidden).PortScan();
             Console.WriteLine("Detected {0} host(s), {1} up and {2} down.", result.Total, result.Up, result.Down);
+            JsonArray machines = new JsonArray();
+            foreach (Host i in result.Hosts)
+            {
+                Console.WriteLine("Host: {0}", i.Address);
+                if (i.OsMatches.Any())
+                {
+                    Console.WriteLine("and is probably running {0}", i.OsMatches.First().Name);
+                }
+                var ports = i.Ports.Select(p=>p.PortNumber).ToArray();
+                
+                var machine = new JsonObject
+                {
+                    ["ip"] = i.Address.ToString(),
+                    ["os"] = i.OsMatches.First().Name,
+                    ["ports"] = JsonSerializer.Serialize(ports),
+                };
+                machines.Add(machine);
+            }
+            File.WriteAllText("Machines.json",machines.ToString());
         }
+        
     }
 }
