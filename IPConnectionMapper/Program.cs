bc94e03862aa00b38c7274983726a927f5abe8ed
netanelka <netanelka@checkpoint.com>
2024-01-18 15:34:27 +0200

    Add nuget packages and write initial functionality

--- IPConnectionMapper/Program.cs
+++ IPConnectionMapper/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using Nmap.NET;
+using Nmap.NET.Container;
+
+namespace IPConnectionManager
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var target = new Target("127.0.0.1");
+            Console.WriteLine("Initializing scan of {0}", target);
+            ScanResult result = new Scanner(target, System.Diagnostics.ProcessWindowStyle.Hidden).PortScan();
+            Console.WriteLine("Detected {0} host(s), {1} up and {2} down.", result.Total, result.Up, result.Down);
+        }
+    }
+}
